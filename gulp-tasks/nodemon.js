f7409fb6a78577a2d0f5792f7ccc58f76787b8b2
Matt Gaunt <matt@gauntface.co.uk>
2015-12-18 15:09:11 +0000

    First crack at fixing #64

--- gulp-tasks/nodemon.js
+++ gulp-tasks/nodemon.js
@@ -1,7 +1,18 @@
 var gulp = require('gulp');
 var nodemon = require('gulp-nodemon');
+var env = require('gulp-env');
 
 gulp.task('nodemon', function() {
+  const PROD_PORT = 8080;
+  const DEV_PORT = 8081;
+  // This results in different ports for prod and dev
+  var port = (GLOBAL.config.env === 'prod') ? PROD_PORT : DEV_PORT;
+  env({
+    vars: {
+      PORT: port
+    }
+  });
+
   return nodemon({
     script: 'app.js'
   });
